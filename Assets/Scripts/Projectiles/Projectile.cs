8fff83556905d6fddc3e3fc16f4bc7f54dc705f8
Bach Nguyen <nlebachnlb.develop@gmail.com>
2026-06-09 22:54:08 +0700

    [misc] upgrade project to unity 6

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -15,7 +15,7 @@ public class Projectile : MonoBehaviour
 
     private void Start()
     {
-        rb.velocity = transform.forward * speed;
+        rb.linearVelocity = transform.forward * speed;
     }
 
     private void Update()
